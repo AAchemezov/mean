3162827a1fa40658da887b5b0eb57fedb843ec86
AAchemezov <a.a.chemezov@gmail.com>
2022-04-08 21:20:54 +0800

    3.30

--- controllers/position.js
+++ controllers/position.js
@@ -3,11 +3,11 @@ const errorHandler = require('../utils/errorHandler')
 
 module.exports.getByCategoryId = async (req, res) => {
     try {
-        const position = await Position.find({
+        const positions = await Position.find({
             category: req.params.categoryId,
             user: req.user.id
         })
-        res.status(200).json(position)
+        res.status(200).json(positions)
     } catch (e) {
         errorHandler(res, e)
     }
