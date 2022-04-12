d4e7604cb865d5054d82b171931c7731e773ca0a
AAchemezov <a.a.chemezov@gmail.com>
2022-04-12 14:09:27 +0800

    5.49

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,7 +9,8 @@ import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.comp
 import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component'
 import {RegisterPageComponent} from './register-page/register-page.component'
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http"
-import {TokenInterceptor} from "./shared/classes/token.interceptor"
+import {TokenInterceptor} from "./shared/classes/token.interceptor";
+import { OverviewPageComponent } from './overview-page/overview-page.component'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import {TokenInterceptor} from "./shared/classes/token.interceptor"
     LoginPageComponent,
     AuthLayoutComponent,
     SiteLayoutComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    OverviewPageComponent
   ],
   imports: [
     BrowserModule,
