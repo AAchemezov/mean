08779d4af66b6afc690fa2a846f5a56714faae3a
AAchemezov <a.a.chemezov@gmail.com>
2022-04-12 10:31:01 +0800

    5.43

--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {AuthService} from "../shared/services/auth.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
-export class RegisterPageComponent implements OnInit {
+export class RegisterPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+  })
 
-  ngOnInit(): void {
+  aSub: Subscription | undefined
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
   }
 
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        // Теперь можно зайти в систему используя свои данные
+      } else if (params['accessDenied']) {
+        // Для начала авторизуйтесь
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.aSub?.unsubscribe()
+  }
+
+  onSubmit() {
+    this.form.disable()
+    this.aSub = this.auth.register(this.form.value).subscribe(
+      () => {
+        console.log('Register success')
+        return this.router.navigate(['/login'],{queryParams: {registered: true}})
+      },
+      e => {
+        console.log(e)
+        this.form.enable()
+      }
+    )
+  }
 }
--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -13,8 +13,8 @@ export class AuthService {
   constructor(private http: HttpClient) {
   }
 
-  register(user: User) {
-
+  register(user: User): Observable<User> {
+    return this.http.post<User>('/api/auth/register', user)
   }
 
   login(user: User): Observable<{ token: string }> {
